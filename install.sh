bbff126e235c5c43edcb334ab8eed6795724afd9
DSuhoi <dsuh0i.h8@gmail.com>
2020-06-29 20:02:38 +0300

    Added more programs in install.sh file.

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 while [ -n "$1" ]
 do
 case "$1" in
--b) sudo pacman -S i3wm i3-gaps i3status rofi pcmanfm feh lxappearance compton xorg-server xorg-xinit xorg-apps mesa-libgl lib32-mesa-libgl xterm xf86-video-intel ;;
--e) sudo pacman -S net-tools firefox libreoffice-fresh libreoffice-fresh-ru ttf-freefont ttf-linux-libertine ttf-hack ttf-font-awesome gimp unzip unrar p7zip vlc pulseaudio pulseaudio-alsa ;;
--c) sudo pacman -S git gcc python jdk-openjdk geany ;;
+-b) sudo pacman -S i3wm i3-gaps i3status rofi vim rxvt-unicode pcmanfm feh lxappearance compton xorg-server xorg-xinit xorg-apps mesa-libgl lib32-mesa-libgl xterm xf86-video-intel ;;
+-e) sudo pacman -S net-tools firefox libreoffice-fresh libreoffice-fresh-ru ttf-freefont ttf-linux-libertine ttf-hack ttf-font-awesome gimp unzip unrar p7zip vlc pulseaudio pulseaudio-alsa links ;;
+-c) sudo pacman -S git gcc python jdk-openjdk geany ranger mc ;;
 -y) 
    sudo pacman -S --needed base-devel git wget yajl
    cd /tmp
