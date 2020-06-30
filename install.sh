9483984f8f13a752db132b9681515c63de4a6299
DSuhoi <dsuh0i.h8@gmail.com>
2020-06-30 15:53:32 +0300

    Updated install.sh file.

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 while [ -n "$1" ]
 do
 case "$1" in
--b) sudo pacman -S i3 i3-gaps i3status rofi vim rxvt-unicode pcmanfm feh lxappearance compton xorg-server xorg-xinit xorg-apps mesa-libgl xterm xf86-video-intel ;;
--e) sudo pacman -S net-tools firefox libreoffice-fresh libreoffice-fresh-ru ttf-freefont ttf-linux-libertine ttf-hack ttf-font-awesome gimp unzip unrar p7zip vlc pulseaudio pulseaudio-alsa links ;;
--c) sudo pacman -S git gcc python jdk-openjdk geany ranger mc ;;
+-b) sudo pacman -S --needed i3 i3-gaps i3status rofi vim rxvt-unicode pcmanfm feh lxappearance compton xorg-server xorg-xinit xorg-apps mesa-libgl xterm xf86-video-intel jq htop ;;
+-e) sudo pacman -S --needed net-tools firefox libreoffice-fresh libreoffice-fresh-ru ttf-freefont ttf-linux-libertine ttf-hack ttf-font-awesome gimp unzip unrar p7zip vlc pulseaudio pulseaudio-alsa links gnome-calculator neofetch;;
+-c) sudo pacman -S --needed git gcc python jdk-openjdk geany ranger mc ;;
 -y) 
    sudo pacman -S --needed base-devel git wget yajl
    cd /tmp
