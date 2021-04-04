bd340e93fc426249fbf11b69175c7276d4c8de1f
dsuhoi <dsuh0i.h8@gmail.com>
2021-04-04 12:14:46 +0300

    Edit install.sh.

--- install.sh
+++ install.sh
@@ -3,8 +3,11 @@
 while [ -n "$1" ]
 do
 case "$1" in
--b) sudo pacman -S --needed vim rxvt-unicode pcmanfm lxappearance compton xorg-server xorg-xinit xorg-apps mesa-libgl xterm xf86-video-intel htop dosfstools ntfs-3g ;;
--e) sudo pacman -S --needed net-tools firefox libreoffice-fresh libreoffice-fresh-ru gimp unzip unrar p7zip vlc pulseaudio pulseaudio-alsa alsa-utils links gnome-calculator neofetch ;;
+-b) sudo pacman -S --needed vim rxvt-unicode pcmanfm lxappearance compton
+    xorg-server xorg-xinit xorg-apps mesa-libgl xterm  xf86-video-intel htop dosfstools ntfs-3g ;;
+-e) sudo pacman -S --needed net-tools firefox libreoffice-fresh
+    libreoffice-fresh-ru gimp unzip unrar p7zip vlc pulseaudio pulseaudio-alsa
+    alsa-utils links gnome-calculator neofetch ;;
 -c) sudo pacman -S --needed git gcc python python2-pip python-pip jdk-openjdk geany ranger mc ;;
 -y) 
    sudo pacman -S --needed base-devel git wget yajl
@@ -26,8 +29,10 @@ case "$1" in
    makepkg -si
    cd ../ && sudo rm -rfv visual-studio-code-bin/ ;;
 -gui)
-   sudo pacman -S --needed i3 i3-gaps i3status rofi feh scrot pacman-contrib arc-gtk-theme jq ttf-freefont ttf-linux-libertine ttf-hack ttf-font-awesome
-   yaourt -S polybar yay numix-circle-arc-icons-git;;
+   sudo pacman -S --needed i3-gaps i3status rofi feh scrot pacman-contrib
+   arc-gtk-theme jq ttf-freefont ttf-linux-libertine ttf-hack terminus-font
+   ttf-font-awesome
+   yaourt -S polybar yay numix-circle-arc-icons-git ;;
 -h)
    echo "-b   --Base installation"
    echo "-e   --Extra installation"
