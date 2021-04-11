1c288eb8686b3e59810b94e1948802709d3df7f4
dsuhoi <dsuh0i.h8@gmail.com>
2021-04-11 12:24:13 +0300

    Update installer.

--- install.sh
+++ install.sh
@@ -7,8 +7,9 @@ case "$1" in
     xorg-server xorg-xinit xorg-apps mesa-libgl xterm  xf86-video-intel htop dosfstools ntfs-3g ;;
 -e) sudo pacman -S --needed net-tools firefox libreoffice-fresh
     libreoffice-fresh-ru gimp unzip unrar p7zip vlc pulseaudio pulseaudio-alsa
-    alsa-utils links gnome-calculator neofetch ;;
--c) sudo pacman -S --needed git gcc python python2-pip python-pip jdk-openjdk geany ranger mc ;;
+    alsa-utils links gnome-calculator neofetch w3m fzf ;;
+-c) sudo pacman -S --needed git gcc python python2-pip python-pip jdk-openjdk
+    ranger ;;
 -y) 
    sudo pacman -S --needed base-devel git wget yajl
    cd /tmp
@@ -21,13 +22,6 @@ case "$1" in
    makepkg -si
    cd ..
    sudo rm -dR yaourt/ package-query/ ;;
--vsc)
-   sudo pacman -S --needed base-devel git
-   cd /tmp
-   git clone https://AUR.archlinux.org/visual-studio-code-bin.git
-   cd visual-studio-code-bin/
-   makepkg -si
-   cd ../ && sudo rm -rfv visual-studio-code-bin/ ;;
 -gui)
    sudo pacman -S --needed i3-gaps i3status rofi feh scrot pacman-contrib
    arc-gtk-theme jq ttf-freefont ttf-linux-libertine ttf-hack terminus-font
@@ -38,7 +32,6 @@ case "$1" in
    echo "-e   --Extra installation"
    echo "-c   --Dev. installation" 
    echo "-y   --AUR installation"
-   echo "-vsc --VS Code instalation"
    echo "-gui --GUI installation" ;;
 *) echo "--parametr ???" ;;
 esac
