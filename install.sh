53da0b526cf18542221496ce3d1083183b4e13ab
dsuhoi <dsuh0i.h8@gmail.com>
2021-07-06 23:52:16 +0300

    Add wayland and sway packages in the install.sh.

--- install.sh
+++ install.sh
@@ -3,11 +3,10 @@
 while [ -n "$1" ]
 do
 case "$1" in
--b) sudo pacman -S --needed vim rxvt-unicode pcmanfm lxappearance compton
-    xorg-server xorg-xinit xorg-apps mesa-libgl xterm  xf86-video-intel htop dosfstools ntfs-3g ;;
+-b) sudo pacman -S --needed vim rxvt-unicode pcmanfm compton xf86-video-intel htop dosfstools ntfs-3g ;;
 -e) sudo pacman -S --needed net-tools firefox libreoffice-fresh
-    libreoffice-fresh-ru gimp unzip unrar p7zip vlc pulseaudio pulseaudio-alsa
-    alsa-utils links gnome-calculator neofetch w3m fzf ;;
+    libreoffice-fresh-ru gimp unzip unrar p7zip cmus pulseaudio pulseaudio-alsa
+    alsa-utils links gnome-calculator neofetch fzf ;;
 -c) sudo pacman -S --needed git gcc python python2-pip python-pip jdk-openjdk
     ranger ;;
 -y) 
@@ -23,10 +22,10 @@ case "$1" in
    cd ..
    sudo rm -dR yaourt/ package-query/ ;;
 -gui)
-   sudo pacman -S --needed i3-gaps i3status rofi feh scrot pacman-contrib
+   sudo pacman -S --needed sway mako rofi feh grim slurp pacman-contrib
    arc-gtk-theme jq ttf-freefont ttf-linux-libertine ttf-hack terminus-font
    ttf-font-awesome
-   yaourt -S polybar yay numix-circle-arc-icons-git ;;
+   yaourt -S yay numix-circle-arc-icons-git ;;
 -h)
    echo "-b   --Base installation"
    echo "-e   --Extra installation"
