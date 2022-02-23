6bd10cb8fa96fd9d09e30bfe32cb1ee45c2d94de
dsuhoi <dsuh0i.h8@gmail.com>
2022-02-23 11:46:39 +0300

    Added wl-clipboard.

--- install.sh
+++ install.sh
@@ -6,24 +6,24 @@ case "$1" in
 -b) sudo pacman -S --needed vim rxvt-unicode alacritty pcmanfm compton xf86-video-intel htop dosfstools ntfs-3g ;;
 -e) sudo pacman -S --needed net-tools firefox libreoffice-fresh libreoffice-fresh-ru gimp unzip unrar p7zip cmus pulseaudio pulseaudio-alsa alsa-utils links gnome-calculator neofetch fzf bat zoxide udiskie;;
 -c) sudo pacman -S --needed git gcc python python-pip jdk-openjdk ranger ;;
--y) 
+-y)
    sudo pacman -S --needed base-devel git
    cd /tmp
    git clone https://aur.archlinux.org/yay.git
-   cd yay  
+   cd yay
    makepkg -si
    cd ..
    sudo rm -Rs yay/ ;;
--bh) 
-    curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh 
+-bh)
+    curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh
     cp .bashrc ~/.bashrc ;;
 -gui)
-   sudo pacman -S --needed sway mako rofi feh grim slurp mutt pacman-contrib arc-gtk-theme jq ttf-freefont ttf-linux-libertine ttf-hack terminus-font ttf-font-awesome gsettings-desktop-schemas noto-fonts-emoji
+   sudo pacman -S --needed sway mako rofi feh grim slurp wl-clipboard mutt pacman-contrib arc-gtk-theme jq ttf-freefont ttf-linux-libertine ttf-hack terminus-font ttf-font-awesome gsettings-desktop-schemas noto-fonts-emoji tesseract
    yay -S numix-circle-arc-icons-git ripgrep;;
 -h)
    echo "-b   --Base installation"
    echo "-e   --Extra installation"
-   echo "-c   --Dev. installation" 
+   echo "-c   --Dev. installation"
    echo "-y   --AUR installation"
    echo "-bh  --Bash installation"
    echo "-gui --GUI installation" ;;
@@ -31,4 +31,3 @@ case "$1" in
 esac
 shift
 done
-
