27592cd72cbb3d729d7e99e54a9b42fa0e159240
dsuhoi <dsuh0i.h8@gmail.com>
2022-11-20 14:30:04 +0700

    Added pip installer in install.sh.

--- install.sh
+++ install.sh
@@ -20,13 +20,16 @@ case "$1" in
 -gui)
    sudo pacman -S --needed sway mako rofi wofi feh grim slurp wl-clipboard mutt pacman-contrib arc-gtk-theme jq ttf-freefont ttf-linux-libertine ttf-hack terminus-font ttf-font-awesome gsettings-desktop-schemas noto-fonts-emoji
    yay -S numix-circle-arc-icons-git ripgrep ;;
+-pip)
+    pip install virtualenv black flake8;;
 -h)
    echo "-b   --Base installation"
    echo "-e   --Extra installation"
    echo "-c   --Dev. installation"
    echo "-y   --AUR installation"
    echo "-bh  --Bash installation"
-   echo "-gui --GUI installation" ;;
+   echo "-gui --GUI installation"
+   echo "-pip --Python modules" ;;
 *) echo "--parametr ???" ;;
 esac
 shift
